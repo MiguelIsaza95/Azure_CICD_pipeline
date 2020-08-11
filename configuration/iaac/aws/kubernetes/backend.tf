d02fdc125daa4c652491eb6eecacb261b0d7e98c
MiguelIsaza95 <miguel11andres@hotmail.com>
2020-08-11 12:48:44 -0500

    Change bucket name

--- configuration/iaac/aws/kubernetes/backend.tf
+++ configuration/iaac/aws/kubernetes/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-mai-azure-tf" # Will be overridden from build
+    bucket = "azure-mai-backend-terraform" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }

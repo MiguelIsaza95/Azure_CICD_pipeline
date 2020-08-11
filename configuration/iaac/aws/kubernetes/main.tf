9d01149c5cbfbba027cee77e762ccc5b26ea415c
MiguelIsaza95 <miguel11andres@hotmail.com>
2020-08-11 11:16:44 -0500

    Change module name

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -29,9 +29,9 @@ provider "kubernetes" {
   version                = "~> 1.9"
 }
 
-module "in28minutes-cluster" {
+module "miguelisaza95-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
+  cluster_name    = "miguelisaza95-cluster"
   cluster_version = "1.14"
   subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
